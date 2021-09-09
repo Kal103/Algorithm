4f0c9baaa259a29cac3d2d616abb50c13295ed5f
Akash671 <akashsaini454545@gmail.com>
2021-09-10 00:35:59 +0530

    add by Akash671

---
+++ string/string_char_count.py
@@ -0,0 +1,17 @@
+s=str(input())
+#aabbccdd
+ans=""
+for i in range(len(set(s))):
+    ans+=s[0]+str(s.count(s[0]))
+    s=s.replace(s[0],"")
+    #remove all s[0] char form s
+print(ans)
+
+#time complexity T(n)=O(n)
+
+"""
+input : 
+aaaabbbccd
+output:
+a4b3c2d1
+"""
